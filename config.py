61bde6efcb711c4c4f137ff45f6934063b552882
silmarasilva <silmara.eliza@gmail.com>
2021-06-01 17:08:02 -0300

    image

--- config.py
+++ config.py
@@ -9,12 +9,12 @@ app.config['BASIC_AUTH_PASSWORD'] = '1234567'
 auth = BasicAuth(app)
 
 mysql = MySQL()
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = '123456' #insira a sua senha do DB
+app.config['MYSQL_DATABASE_USER'] = 'silale'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'silale2021' #insira a sua senha do DB
 app.config['MYSQL_DATABASE_DB'] = 'db_clientes' #insira o nome do seu DB
 app.config['MYSQL_DATABASE_DB'] = 'db_produtos' #insira o nome do seu DB
 app.config['MYSQL_DATABASE_DB'] = 'db_vendas' #insira o nome do seu DB
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_HOST'] = 'db-silale.cxycaymkd24m.us-east-1.rds.amazonaws.com'
 
 
 mysql.init_app(app)
--- main_vendas.py
+++ main_vendas.py
@@ -28,8 +28,8 @@ def add_compra():
             cursor = conn.cursor(pymysql.cursors.DictCursor)
 
             # Verificação se o Id do cliente confere com o db_clientes
-            cliente = requests.get(f'http://127.0.0.1:5000/clientes/{_idCliente}', headers = {"Authorization":"Basic c2lsc2lsOjEyMzQ1Njc="})
-            curso = requests.get(f'http://127.0.0.1:5200/cursos/{_idCurso}', headers = {"Authorization":"Basic c2lsc2lsOjEyMzQ1Njc="})
+            cliente = requests.get(f'DNS/clientes/{_idCliente}', headers = {"Authorization":"Basic c2lsc2lsOjEyMzQ1Njc="})
+            curso = requests.get(f'DNS/cursos/{_idCurso}', headers = {"Authorization":"Basic c2lsc2lsOjEyMzQ1Njc="})
             #status = requests.get(f'http://127.0.0.1:5200/cursos/status/{_idCurso}', headers = {"Authorization":"Basic c2lsc2lsOjEyMzQ1Njc="})         
 
             # Verificando se o cliente e o curso existem:                       
@@ -94,12 +94,12 @@ def id_compras (idCliente):
         # Buscando os dados dos clientes e do curso que constam no inventário (userRow) - JOIN        
         curso = [] #lista de json dos vários cursos comprados por um cliente
         #cliente = requests.get(f'http://127.0.0.1:5000/clientes/{idCliente}', headers = {"Authorization":"Basic c2lsc2lsOjEyMzQ1Njc="})
-        cliente = requests.get('http://127.0.0.1:5000/clientes/{}'.format(idCliente), headers = {"Authorization":"Basic c2lsc2lsOjEyMzQ1Njc="})
+        cliente = requests.get('DNS/clientes/{}'.format(idCliente), headers = {"Authorization":"Basic c2lsc2lsOjEyMzQ1Njc="})
         for i in (userRow):
             #j = i['idCurso']
             #c = requests.get(f'http://127.0.0.1:5200/cursos/{j}', headers = {"Authorization":"Basic c2lsc2lsOjEyMzQ1Njc="})
             #c = requests.get('http://127.0.0.1:5200/cursos/{}'.format(i['idCurso']), headers = {"Authorization":"Basic c2lsc2lsOjEyMzQ1Njc="})
-            c = requests.get(f'http://127.0.0.1:5200/cursos/{i["idCurso"]}', headers = {"Authorization":"Basic c2lsc2lsOjEyMzQ1Njc="})
+            c = requests.get(f'DNS/cursos/{i["idCurso"]}', headers = {"Authorization":"Basic c2lsc2lsOjEyMzQ1Njc="})
             i["data"] = f"{i['data']}"
             curso.append(c.json())
 
@@ -136,8 +136,8 @@ def update_curso(id):
             sqlQuery = "UPDATE tbl_cliente_compra_cursos SET data=%s, idCliente=%s, idCurso=%s, idCompra=%s WHERE idCompra=%s"
             bindData = (_data, _idCliente, _idCurso, _idCompra, id)
             # Verificação se o Id do cliente confere com o db_clientes
-            cliente = requests.get(f'http://127.0.0.1:5000/clientes/{_idCliente}', headers = {"Authorization":"Basic c2lsc2lsOjEyMzQ1Njc="})            
-            curso = requests.get(f'http://127.0.0.1:5200/cursos/{_idCurso}', headers = {"Authorization":"Basic c2lsc2lsOjEyMzQ1Njc="})
+            cliente = requests.get(f'DNS/clientes/{_idCliente}', headers = {"Authorization":"Basic c2lsc2lsOjEyMzQ1Njc="})            
+            curso = requests.get(f'DNS/cursos/{_idCurso}', headers = {"Authorization":"Basic c2lsc2lsOjEyMzQ1Njc="})
                        
             if cliente.status_code == 404:
                 return ('Cliente não encontrado'), 400
@@ -192,4 +192,4 @@ def not_found(error=None):
     return response
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5100)
+    app.run(debug=True, host = "0.0.0.0", port = 80)
